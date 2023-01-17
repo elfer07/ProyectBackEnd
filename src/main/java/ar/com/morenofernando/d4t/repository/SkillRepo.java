b6a66c1ce91c91d2dbcc58acf233fc01eb5382a6
elfer07 <morenofernando@gmail.com>
2023-01-16 23:36:03 -0300

    backend

--- src/main/java/ar/com/morenofernando/d4t/repository/SkillRepo.java
+++ src/main/java/ar/com/morenofernando/d4t/repository/SkillRepo.java
@@ -2,9 +2,11 @@ package ar.com.morenofernando.d4t.repository;
 
 import ar.com.morenofernando.d4t.entity.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface SkillRepo extends JpaRepository<Skill, Integer> {
     Optional<Skill> findByNombre(String nombre);
     public boolean existsByNombre(String nombre);
